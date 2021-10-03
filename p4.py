21d0ca80b9c5347d337f27481c626a2240ee134f
Kumar Sheetanshu <51575875+sheetanshu11@users.noreply.github.com>
2021-10-04 00:15:53 +0530

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#function that takes 0 argument and returns 1 argument
+
+def hello():
+	tmp1='Hello World'
+	return tmp1
+
+def main():
+	var1=hello()
+	print(var1)
+
+if __name__=='__main__':
+	main()
