216141e5fb3907ecc5a50fb6d9f7bfc1c7a70008
Kumar Sheetanshu <51575875+sheetanshu11@users.noreply.github.com>
2021-10-04 00:13:57 +0530

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#function that takes n arguments and returns 0 argument
+def hello(var1,var2):
+	print('Hello '+var1+' '+var2)
+def main():
+	var1='ram'
+	var2='shyam'
+#	hello(var1,var2)
+
+if __name__=='__main__':
+	main()
